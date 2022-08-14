8e6d618dff8add268b6b363fbc64d0e5be5651b6
sunriseyuen <tiptoes@foxmail.com>
2022-08-14 09:04:38 +0800

    Update url.ts

--- src/api/url.ts
+++ src/api/url.ts
@@ -10,7 +10,7 @@ export const updateUserInfo = '/updateUser'
 
 export const addUserInfo = '/addUser'
 
-export const getMenuListByRoleId = '/getMenusByRoleId'
+export const getMenuListByRoleId = '/ADM04017B'
 
 export const getAllMenuByRoleId = '/getAllMenuByRoleId'
 

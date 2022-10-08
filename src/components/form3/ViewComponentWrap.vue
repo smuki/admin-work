2bf8dff718d43d21253501d3886f5d1b61c17fa7
sunriseyuen <tiptoes@foxmail.com>
2022-10-08 09:25:18 +0800

    Update ViewComponentWrap.vue

--- src/components/form3/ViewComponentWrap.vue
+++ src/components/form3/ViewComponentWrap.vue
@@ -62,7 +62,10 @@
 </template>
 
 <script>
-import { SchemaField, globalOptions } from '@lljj/vue-json-schema-form';
+    
+import { SchemaField, globalOptions } from '@/vue3-form-naive';
+import getDefaultFormState  from '@/utils/schema/getDefaultFormState';
+
 import emitter from '../../../mixins/emitter.js';
 import NestedEditor from './NestedEditor';
 import { editorItem2SchemaFieldProps } from '../common/editorData';

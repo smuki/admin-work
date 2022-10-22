a704aac357c27b4fe8120c6ca467f35aea2deb99
sunriseyuen <tiptoes@foxmail.com>
2022-10-22 15:28:27 +0800

    Update index.js

--- src/views/form/Naive/index.js
+++ src/views/form/Naive/index.js
@@ -2,10 +2,10 @@
  * Created by Liu.Jun on 2021/2/1 10:00 下午.
  */
 
-import NaiveUI from 'naive-ui';
+import NaiveUI from 'naive-ui'
 
 export default {
-    install(app) {
-        app.use(NaiveUI);
-    }
-};
+  install(app) {
+    app.use(NaiveUI)
+  },
+}

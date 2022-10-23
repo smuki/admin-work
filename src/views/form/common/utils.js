f8367623ce97e9eb173c2b3cb94f076e915c6d4a
sunriseyuen <tiptoes@foxmail.com>
2022-10-23 20:56:48 +0800

    update

--- src/views/form/common/utils.js
+++ src/views/form/common/utils.js
@@ -5,30 +5,30 @@
 // import { genId } from 'demo-common/utils/id';
 
 export function isObject(obj) {
-    return (Object.prototype.toString.call(obj) === '[object Object]');
+  return Object.prototype.toString.call(obj) === '[object Object]'
 }
 
 export function isEmptyObject(obj) {
-    for (const key in obj) {
-        if (Object.prototype.hasOwnProperty.call(obj, key)) {
-            return false;
-        }
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false
     }
-    return true;
+  }
+  return true
 }
 
 export function deepFreeze(obj) {
-    // 取回定义在obj上的属性名
-    const propNames = Object.getOwnPropertyNames(obj);
+  // 取回定义在obj上的属性名
+  const propNames = Object.getOwnPropertyNames(obj)
 
-    // 在冻结自身之前冻结属性
-    propNames.forEach((name) => {
-        const prop = obj[name];
+  // 在冻结自身之前冻结属性
+  propNames.forEach((name) => {
+    const prop = obj[name]
 
-        // 如果prop是个对象，冻结它
-        if (typeof prop === 'object' && prop !== null) deepFreeze(prop);
-    });
+    // 如果prop是个对象，冻结它
+    if (typeof prop === 'object' && prop !== null) deepFreeze(prop)
+  })
 
-    // 冻结自身(no-op if already frozen)
-    return Object.freeze(obj);
+  // 冻结自身(no-op if already frozen)
+  return Object.freeze(obj)
 }
